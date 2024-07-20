1c7decfe45911a003b41361468f269572d2b74e3
danishirfan21 <44131991+danishirfan21@users.noreply.github.com>
2024-07-21 00:58:58 +0500

    added open ai api integration and some fixes

--- src/components/Translation/index.js
+++ src/components/Translation/index.js
@@ -1,62 +1,120 @@
 import React, { useState } from "react";
+import OpenAI from "openai";
 import "./styles.css";
-import { VIEW_STATES } from "./constants";
 
 export default function Translation() {
-  const [viewState, setViewState] = useState(VIEW_STATES.INITIAL);
+  const [isInitialState, setIsInitialState] = useState(true);
+  const [translatedText, setTranslatedText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(!isInitialState) {
+      setIsInitialState(prev => !prev);
+      setTranslatedText('')
+      // form.reset();
+      return;
+    }
+
+    const getForm = e.target;
+    const formData = new FormData(getForm);
+
+    const textToTranslate = formData.get("textToTranslate");
+    const language = formData.get("language");
+
+    const openai = new OpenAI({
+      dangerouslyAllowBrowser: true,
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    });
+
+    const messages = [
+      {
+        role: "system",
+        content:
+          "Act like you are an expert in translating text of any language.",
+      },
+      {
+        role: "user",
+        content: `Translate this given english text: "${textToTranslate}" in ${language} language`,
+      },
+    ];
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: messages,
+    });
+
+    setIsInitialState((prev) => !prev);
+    setTranslatedText(response.choices[0].message.content);
+  };
 
   return (
-    <div className="translation-container">
-      <div className="input-section">
-        <h3 className="input-title">{`${
-          viewState === VIEW_STATES.INITIAL
-            ? "Text to translate"
-            : "Original text"
-        } 👇`}</h3>
-        <textarea rows={5} placeholder="Type here..." className="textarea" />
-      </div>
-      <div className="output-section">
-        <h3 className="output-title">{`${
-          viewState === VIEW_STATES.INITIAL
-            ? "Select language"
-            : "Your translation"
-        } 👇`}</h3>
-        <div className="language-options">
-          <input
-            type="radio"
-            id="languageChoice1"
-            name="language"
-            value="french"
+    <form method="POST" onSubmit={handleSubmit}>
+      <div className="translation-container">
+        <div className="input-section">
+          <h3 className="input-title">{`${
+            isInitialState ? "Text to translate" : "Original text"
+          } 👇`}</h3>
+          <textarea
+            name="textToTranslate"
+            rows={5}
+            placeholder="Type here..."
+            className="textarea"
+            readOnly={!isInitialState}
           />
-          <label for="languageChoice1">
-            French <img src="/assets/fr-flag.png" alt="france-flag" />
-          </label>
-
-          <input
-            type="radio"
-            id="languageChoice2"
-            name="language"
-            value="spanish"
-          />
-          <label for="languageChoice2">
-            Spanish <img src="/assets/sp-flag.png" alt="spain-flag" />
-          </label>
-
-          <input
-            type="radio"
-            id="languageChoice3"
-            name="language"
-            value="japanese"
-          />
-          <label for="languageChoice3">
-            Japanese <img src="/assets/jpn-flag.png" alt="japan-flag" />
-          </label>
         </div>
-      </div>
+        <div className="output-section">
+          <h3 className="output-title">{`${
+            isInitialState ? "Select language" : "Your translation"
+          } 👇`}</h3>
+          {isInitialState ? (
+            <div className="language-options">
+              <input
+                type="radio"
+                id="languageChoice1"
+                name="language"
+                value="french"
+                defaultChecked
+              />
+              <label htmlFor="languageChoice1">
+                French <img src="/assets/fr-flag.png" alt="france-flag" />
+              </label>
 
-      <button className="translate-button">
-        {viewState === VIEW_STATES.INITIAL ? "Translate" : "Start Over"}
-      </button>
-    </div>
+              <input
+                type="radio"
+                id="languageChoice2"
+                name="language"
+                value="spanish"
+              />
+              <label htmlFor="languageChoice2">
+                Spanish <img src="/assets/sp-flag.png" alt="spain-flag" />
+              </label>
+
+              <input
+                type="radio"
+                id="languageChoice3"
+                name="language"
+                value="japanese"
+              />
+              <label htmlFor="languageChoice3">
+                Japanese <img src="/assets/jpn-flag.png" alt="japan-flag" />
+              </label>
+            </div>
+          ) : (
+            <textarea
+              rows={5}
+              placeholder="Type here..."
+              className="textarea"
+              value={translatedText}
+              readOnly
+            />
+          )}
+        </div>
+
+        <button className="translate-button">
+          {isInitialState ? "Translate" : "Start Over"}
+        </button>
+      </div>
+    </form>
   );
 }
