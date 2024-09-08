459c7afd3fa6c70a17a8ef69f2ce233e045b8aab
danishirfan21 <44131991+danishirfan21@users.noreply.github.com>
2024-09-08 23:30:59 +0500

    updated code for pollyglot

--- src/components/Translation/index.js
+++ src/components/Translation/index.js
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import OpenAI from "openai";
-import "./styles.css";
+import React, { useState } from 'react';
+import OpenAI from 'openai';
+import './styles.css';
 
 export default function Translation() {
   const [isInitialState, setIsInitialState] = useState(true);
-  const [translatedText, setTranslatedText] = useState("");
+  const [textToTranslate, setTextToTranslate] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!isInitialState) {
-      setIsInitialState(prev => !prev);
-      setTranslatedText('')
-      // form.reset();
+    if (isInitialState && textToTranslate === '') {
+      alert('Please enter text to translate');
+      return;
+    }
+
+    if (!isInitialState) {
+      setIsInitialState((prev) => !prev);
+      setTranslatedText('');
+      setTextToTranslate('');
       return;
     }
 
     const getForm = e.target;
     const formData = new FormData(getForm);
 
-    const textToTranslate = formData.get("textToTranslate");
-    const language = formData.get("language");
+    const language = formData.get('language');
 
     const openai = new OpenAI({
       dangerouslyAllowBrowser: true,
@@ -29,18 +34,18 @@ export default function Translation() {
 
     const messages = [
       {
-        role: "system",
+        role: 'system',
         content:
-          "Act like you are an expert in translating text of any language.",
+          'Act like you are an expert in translating text of any language.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `Translate this given english text: "${textToTranslate}" in ${language} language`,
       },
     ];
 
     const response = await openai.chat.completions.create({
-      model: "gpt-4",
+      model: 'gpt-4',
       messages: messages,
     });
 
@@ -53,10 +58,12 @@ export default function Translation() {
       <div className="translation-container">
         <div className="input-section">
           <h3 className="input-title">{`${
-            isInitialState ? "Text to translate" : "Original text"
+            isInitialState ? 'Text to translate' : 'Original text'
           } 👇`}</h3>
           <textarea
             name="textToTranslate"
+            value={textToTranslate}
+            onChange={(e) => setTextToTranslate(e.target.value)}
             rows={5}
             placeholder="Type here..."
             className="textarea"
@@ -65,38 +72,38 @@ export default function Translation() {
         </div>
         <div className="output-section">
           <h3 className="output-title">{`${
-            isInitialState ? "Select language" : "Your translation"
+            isInitialState ? 'Select language' : 'Your translation'
           } 👇`}</h3>
           {isInitialState ? (
             <div className="language-options">
-              <input
-                type="radio"
-                id="languageChoice1"
-                name="language"
-                value="french"
-                defaultChecked
-              />
               <label htmlFor="languageChoice1">
+                <input
+                  type="radio"
+                  id="languageChoice1"
+                  name="language"
+                  value="french"
+                  defaultChecked
+                />
                 French <img src="/assets/fr-flag.png" alt="france-flag" />
               </label>
 
-              <input
-                type="radio"
-                id="languageChoice2"
-                name="language"
-                value="spanish"
-              />
               <label htmlFor="languageChoice2">
+                <input
+                  type="radio"
+                  id="languageChoice2"
+                  name="language"
+                  value="spanish"
+                />
                 Spanish <img src="/assets/sp-flag.png" alt="spain-flag" />
               </label>
 
-              <input
-                type="radio"
-                id="languageChoice3"
-                name="language"
-                value="japanese"
-              />
               <label htmlFor="languageChoice3">
+                <input
+                  type="radio"
+                  id="languageChoice3"
+                  name="language"
+                  value="japanese"
+                />
                 Japanese <img src="/assets/jpn-flag.png" alt="japan-flag" />
               </label>
             </div>
@@ -112,7 +119,7 @@ export default function Translation() {
         </div>
 
         <button className="translate-button">
-          {isInitialState ? "Translate" : "Start Over"}
+          {isInitialState ? 'Translate' : 'Start Over'}
         </button>
       </div>
     </form>
